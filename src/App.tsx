921502fc0dcd4245fab8e835c16d675e93c74d49
shinyunsung <114058540+Dev-ShinY@users.noreply.github.com>
2024-02-07 18:47:05 +0900

    :package: Add tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./styles/App.css";
+import clsx from "clsx";
+import React from "react";
+
+function App() {
+  return <div className={clsx("p-5", "font-bold")}>hello weather</div>;
+}
+
+export default App;
